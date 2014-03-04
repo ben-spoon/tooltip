c1b6d6935c3ed16a02ea2b74019c18621c24671e
Ben Spoon <bens@aweber.com>
2014-03-04 15:47:57 -0500

    Work in progress
    On it's way but not there
    quite yet. This could use
    a few more revisions before
    I merge it in.
    
    Adds
      * Image handling
      * Hovering over tooltips for
        links and such
      * Start of positioning

--- tooltip.js
+++ tooltip.js
@@ -1,16 +1,57 @@
+/**
+ * Adds a tooltip to an object
+ * Uses the data-title of that object for the
+ * tooltip text
+ *
+ * Ben Spoon
+ * http://benspoon.com
+ * @benspoon
+ */
+ (function() {
+var clear = null;
 $(document).on('mouseover', '.tooltip', function() {
-    /**
-     * Adds a tooltip to an object
-     * Uses the data-title of that object for the
-     * tooltip text
-     */
-
     $('.tooltip-wrapper').remove();
     var text = $(this).data('title'),
+        imageOptions = $(this).data('image'),
+        style = $(this).data('position'),
+        imageSplit = '',
+        imageStyle = '',
         pos   = $(this).offset();
-    $(this).append('<div class="tooltip-wrapper"><span class="tool-tip-text">' + text + '</span></div>');
-    $('.tooltip-wrapper').css('top', pos.top + 15).css('left', pos.left + 15);
+
+    $(this).parent().append('<div class="tooltip-wrapper"></div>');
+    if (imageOptions) {
+        if (imageOptions.indexOf(',') > -1) {
+            imageSplit = imageOptions.split(',');
+            image = imageSplit[0];
+            imageStyle = imageSplit[1];
+        } else {
+            image = imageOptions;
+        }
+        $('.tooltip-wrapper').append('<img style="'+
+            imageStyle+'" src="'+image+'" class="tool-tip-image"></img>');
+    }
+    if (text !== '') {
+        $('.tooltip-wrapper').append('<span class="tool-tip-text">'+
+            text + '</span>');
+    }
+    if (style === 'bottom') {
+        $('.tooltip-wrapper').css('margin-top', 5).css('left', pos.left + 30);
+    } else if (style === 'top') {
+        $('.tooltip-wrapper').css('margin-top', -($('.tooltip-wrapper').height()*4)).css('left', pos.left + 30);
+    }
 
 }).on('mouseout', '.tooltip', function() {
-    $('.tooltip-wrapper').remove();
+    clear = setTimeout(function (){
+        $('.tooltip-wrapper').remove();
+    }, 100);
+}).on('mouseenter', '.tooltip-wrapper', function() {
+    if (clear){
+        clearTimeout(clear);
+        clear = null;
+    }
+}).on('mouseleave', '.tooltip-wrapper', function() {
+    clear = setTimeout(function (){
+        $('.tooltip-wrapper').remove();
+    }, 100);
 });
+})();
